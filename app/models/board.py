7bf201196cdf5b6f0e560bcfd699e693fc325df7
annadu <duyuan0921@gmail.com>
2023-02-06 21:07:56 -0800

    board model done

--- app/models/board.py
+++ app/models/board.py
@@ -1,1 +1,19 @@
 from app import db
+
+
+class Board(db.Model):
+    board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    owner = db.Column(db.String)
+    cards = db.relationship("Card", back_populates="board")
+
+    def to_dict(self):
+        board_dict = {
+            "board_id" : self.board_id,
+            "owner": self.owner,
+            "title": self.title,
+        }
+        if self.cards:
+            board_dict["cards"] = [card.to_dict() for card in self.cards]
+
+        return board_dict
